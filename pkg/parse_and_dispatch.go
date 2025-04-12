d25120da154377e6813d9472a0b3b7ffe8104692
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-04-12 22:57:03 +0200

    add saving to the database functionality to the init mode

--- pkg/parse_and_dispatch.go
+++ pkg/parse_and_dispatch.go
@@ -1,15 +1,26 @@
 package pkg
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"slices"
+	"time"
+
+	// "oma/internal/db"
+	// "oma/internal/db/repositories"
+	"oma/internal/db/models"
+	"oma/internal/db/postgres"
+	"oma/internal/db/repositories"
+
+	"github.com/jmoiron/sqlx"
 )
 
 var OMA_IGNORE_DEFAULTS = []string{".git", ".oma", ".omaignore", ".gitignore"}
 
-func ParseAndDispatch(args []string) {
+func ParseAndDispatch(args []string, dbIns *sqlx.DB) {
+	defer dbIns.Close()
 	if slices.Contains(args, "init") {
 		if len(args) > 2 {
 			log.Fatal("illogical flags/commands type oma init --help for usage")
@@ -25,7 +36,26 @@ func ParseAndDispatch(args []string) {
 		var fileIngredients []FileIngredients
 		WalkDirs(currDir, &fileIngredients, []string{}, ignoreList)
 
-		fmt.Printf("fileIngredients: %+v\n", fileIngredients)
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		omaRepo := postgres.NewOmaRepository(dbIns)
+		repoContainer := repositories.RepositoryContainer{
+			OmaRepository: omaRepo,
+		}
+
+		for _, entry := range fileIngredients {
+
+			createdRepo, err := repoContainer.OmaRepository.Create(ctx, &models.OmaRepository{
+				FileName:   &entry.fileName,
+				CachedText: &entry.content,
+			})
+			check(err, true)
+
+			fmt.Printf("saved: %v\n", *createdRepo.CachedText)
+		}
+
+		log.Print("repository initialized succesfully")
 		return
 	}
 
