704887bac1e76da3bae15a9752ba804fe0e6efe6
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-04-12 14:01:12 +0200

    improve migration runner script with a reset mode

--- cmd/migrate/runner.go
+++ cmd/migrate/runner.go
@@ -10,26 +10,46 @@ import (
 	"path/filepath"
 )
 
-func reset(path string, dbUrl string) {
-	m, err := migrate.New(
-		path,
-		dbUrl,
-	)
-
+func reset(m *migrate.Migrate, path string, dbUrl string) {
+	version, dirty, err := m.Version()
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 
-	if err := m.Force(0); err != nil {
-		log.Fatal(err)
+	log.Printf("current version: %v, is dirty: %v", version, dirty)
+
+	if version > 0 && dirty == false {
+		if err := m.Drop(); err != nil {
+			log.Print("Yo:", err)
+		}
+
+		log.Print("Dropped everything")
+
+		m.Close()
+		m, err = migrate.New(path, dbUrl)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Print("schema migrations table initiated again")
 	}
 
 	if err := m.Up(); err != nil {
-		log.Fatal()
+		log.Fatal(err)
+	}
+}
+
+func runMigrations(m *migrate.Migrate) {
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
 	}
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("usage: run-command <mode: 'reset' | 'migrate'>")
+	}
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Environment variables could not be load!")
@@ -60,8 +80,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := m.Up(); err != nil {
-		log.Fatal(err)
+	if os.Args[1] == "reset" {
+		reset(m, path, dbUrl)
+	} else if os.Args[1] == "migrate" {
+		runMigrations(m)
+	} else {
+		log.Fatalf("invalid mode, must be 'reset' or 'migrate'\nExiting...")
 	}
 
 	log.Printf("Migrations ran successfully!")
