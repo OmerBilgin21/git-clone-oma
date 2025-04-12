2383a0b1d13e8c3970f3eb12363b19a01dbe9e62
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-04-12 22:54:52 +0200

    add create func to oma repo impl

--- internal/db/postgres/oma_repository.go
+++ internal/db/postgres/oma_repository.go
@@ -29,3 +29,18 @@ func NewOmaRepository(db *sqlx.DB) *OmaRepositoryImpl {
 	return &OmaRepositoryImpl{db: db}
 }
 
+func (r *OmaRepositoryImpl) Create(ctx context.Context, data *models.OmaRepository) (*models.OmaRepository, error) {
+	query := `insert into repositories (cached_text, filename) 
+	values ($1, $2) 
+	returning *`
+
+	createdRepo := &models.OmaRepository{}
+
+	fmt.Printf("data.CachedText: %v\n", *data.CachedText)
+	err := r.db.GetContext(ctx, createdRepo, query, *data.CachedText, *data.FileName)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return createdRepo, err
+}
