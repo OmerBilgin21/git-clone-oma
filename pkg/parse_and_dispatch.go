f28489a3de20d188f4ddfe72dab746a9bc358d53
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-04-12 02:41:56 +0200

    rename file and rename constant

--- pkg/parse_and_dispatch.go
+++ pkg/parse_and_dispatch.go
@@ -2,14 +2,20 @@ package pkg
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"slices"
 )
 
-var OmaIgnoreDefaults = []string{".git", ".oma", ".omaignore", ".gitignore"}
+var OMA_IGNORE_DEFAULTS = []string{".git", ".oma", ".omaignore", ".gitignore"}
 
 func ParseAndDispatch(args []string) {
 	if slices.Contains(args, "init") {
+		if len(args) > 2 {
+			log.Fatal("illogical flags/commands type oma init --help for usage")
+		} else if len(args) == 2 && args[1] == "--help" {
+			log.Fatal("help docs, TBD")
+		}
 
 		currDir, err := os.Getwd()
 		check(err, true)
