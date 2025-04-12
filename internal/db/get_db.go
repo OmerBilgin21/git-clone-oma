d6c3070d5c3297fea072cb11267b385711924768
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-04-12 22:56:24 +0200

    add initialize db func with sqlx

---
+++ internal/db/get_db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+func GetDb() *sqlx.DB {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("env vars could not load: %v", err)
+	}
+
+	db, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("error while connection to db: %s", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("something went wrong while pinging the DB: %s", err)
+	}
+
+	return db
+}
