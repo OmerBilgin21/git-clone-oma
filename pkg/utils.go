49e795800916df6512b42a97f3f2b17a3631e653
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-04-26 19:00:39 +0200

    do not export trivial functions and format

--- pkg/utils.go
+++ pkg/utils.go
@@ -7,8 +7,19 @@ import (
 	"strings"
 )
 
-func PurifyReadResult(lines []string) []string {
+func walkDirsAndReadFiles() []FileIngredients {
+	currDir, err := os.Getwd()
+	check(err, true)
 
+	ignoreList := parseOmaIgnore()
+
+	var fileIngredients []FileIngredients
+	WalkDirs(currDir, &fileIngredients, []string{}, ignoreList)
+
+	return fileIngredients
+}
+
+func purifyReadResult(lines []string) []string {
 	if len(lines) > 0 {
 		for i, elem := range lines {
 			if elem == "" || elem == " " || elem == "\n" {
@@ -20,18 +31,16 @@ func PurifyReadResult(lines []string) []string {
 	return lines
 }
 
-func ParseOmaIgnore() []string {
+func parseOmaIgnore() []string {
 	omaIgnoreBytes, err := os.ReadFile("./.omaignore")
 	check(err, false)
 	omaIgnore := string(omaIgnoreBytes)
 
-	separatedArgs := PurifyReadResult(strings.Split(omaIgnore, "\n"))
+	separatedArgs := purifyReadResult(strings.Split(omaIgnore, "\n"))
 	separatedArgs = append(separatedArgs, OMA_IGNORE_DEFAULTS...)
 
 	return separatedArgs
 }
-
-func FindIndex() {}
 
 func check(err error, fail bool) {
 	if err != nil {
