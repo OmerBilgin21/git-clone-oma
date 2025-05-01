9fe13b9b1f14d950940359eb1d94ef772f248504
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-01 16:23:37 +0200

    add the new version id field to version entity

--- internal/storage/version_entity.go
+++ internal/storage/version_entity.go
@@ -9,5 +9,6 @@ type Versions struct {
 	ID           int          `db:"id"`
 	CreatedAt    time.Time    `db:"created_at"`
 	DeletedAt    sql.NullTime `db:"deleted_at"`
+	VersionId    int          `db:"version_id"`
 	RepositoryId int          `db:"repository_id"`
 }
