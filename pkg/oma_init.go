a6b502c5db2b8964be1a2cd0bb3bf00e357b03cc
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-01 16:26:14 +0200

    add local state file creation to init command

--- pkg/oma_init.go
+++ pkg/oma_init.go
@@ -3,27 +3,49 @@ package pkg
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"oma/internal/storage"
+	"strings"
 )
 
 func GitInit(ctx context.Context, repoContainer *storage.RepositoryContainer, fileIngredients *[]FileIngredients) error {
-	for _, entry := range *fileIngredients {
-		_, err := repoContainer.OmaRepository.Create(ctx, &storage.OmaRepository{
+	var randomCreatedRepo *storage.OmaRepository
+	nextId, err := repoContainer.OmaRepository.GetNextOmaRepoId(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	for i, entry := range *fileIngredients {
+		createdRepo, err := repoContainer.OmaRepository.Create(ctx, &storage.OmaRepository{
 			FileName: sql.NullString{
 				String: entry.fileName,
 				Valid:  true,
 			},
-
 			CachedText: sql.NullString{
 				String: entry.content,
 				Valid:  true,
 			},
+			OmaRepoId: nextId,
 		})
+
+		if i == 0 {
+			randomCreatedRepo = createdRepo
+		}
 
 		if err != nil {
 			return err
 		}
+	}
+
+	err = repoContainer.FileIORepository.CreateRepoInitInfo(randomCreatedRepo.OmaRepoId)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "exists") {
+			return fmt.Errorf("can not initialize repository, it is already done")
+		}
+		return fmt.Errorf("error while creating repo init file:\n%v", err)
 	}
 
 	log.Print("repository initialized succesfully")
