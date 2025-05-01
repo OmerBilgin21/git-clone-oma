78e3bcec007ad29334ccb603c7bac34d96129e0a
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-01 16:26:30 +0200

    remove redundant usage of the filenames

--- pkg/render_diffs.go
+++ pkg/render_diffs.go
@@ -18,12 +18,15 @@ func isVisuallyEmpty(s string) bool {
 // I couldn't find a nice table rendering display tool for large text files
 // (or couldn't make tablewriter work in other words)
 // so here we go:
-func renderSideBySideDiff(oldColoured, newColoured, oldName, newName string) {
+func renderSideBySideDiff(oldColoured, newColoured, filename string) {
 	separator := Reset + " | " + Reset
 	oldLines, newLines := strings.Split(oldColoured, "\n"), strings.Split(newColoured, "\n")
 	maxLines := max(len(oldLines), len(newLines))
 
-	fmt.Printf("\n%s%s%s\n", consolidateShortLine(filepath.Base(oldName)), separator, filepath.Base(newName))
+	filenameBasePath := filepath.Base(filename)
+	headerName := consolidateShortLine(filenameBasePath)
+
+	fmt.Printf("\n%s%s%s\n", headerName, separator, headerName)
 
 	columnSeparator := strings.Repeat("-", Width*2)
 	fmt.Printf(columnSeparator + "\n")
@@ -39,16 +42,16 @@ func renderSideBySideDiff(oldColoured, newColoured, oldName, newName string) {
 	}
 }
 
-func RenderDiffs(oldContent, newContent, oldName, newName string) error {
+func RenderDiffs(oldContent, newContent, filename string) error {
 	additions, deletions, moves, normalizedOld, normalizedNew, err := GetDiff(oldContent, newContent)
 
 	if err != nil {
-		return fmt.Errorf("diff view would be broken therefore it won't be shown for this file: %v", err)
+		return fmt.Errorf("diff view would be broken therefore it won't be shown for this file: %v\n", err)
 	}
 
 	if len(additions) > 0 || len(deletions) > 0 || len(moves) > 0 {
 		oldColoured, newColoured := ColourTheDiffs(additions, deletions, moves, normalizedOld, normalizedNew)
-		renderSideBySideDiff(oldColoured, newColoured, oldName, newName)
+		renderSideBySideDiff(oldColoured, newColoured, filename)
 	}
 
 	return nil
