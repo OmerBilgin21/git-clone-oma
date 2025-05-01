cf9a98aec73c93ed7ccbb72817e20756dec617c6
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-01 16:20:20 +0200

    add repository-filename uniqueness constraint to the filename additiona migration

--- internal/migrations/0002_add_filename.up.sql
+++ internal/migrations/0002_add_filename.up.sql
@@ -1,2 +1,7 @@
-alter table repositories add column filename varchar not null
+alter table repositories add column filename varchar not null;
+
+ALTER TABLE repositories
+ADD CONSTRAINT
+oma_repo_id_unique_for_each_filename
+UNIQUE(oma_repo_id, filename)
 
