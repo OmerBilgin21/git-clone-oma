481ff0425dab3cf3b2e08b4942f02dc5d2385315
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-01 16:22:32 +0200

    add oma repo id to oma repository entity

--- internal/storage/oma_repository_entity.go
+++ internal/storage/oma_repository_entity.go
@@ -8,6 +8,7 @@ import (
 type OmaRepository struct {
 	ID         int            `db:"id"`
 	CreatedAt  time.Time      `db:"created_at"`
+	OmaRepoId  int            `db:"oma_repo_id"`
 	DeletedAt  sql.NullTime   `db:"deleted_at"`
 	FileName   sql.NullString `db:"filename"`
 	CachedText sql.NullString `db:"cached_text"`
