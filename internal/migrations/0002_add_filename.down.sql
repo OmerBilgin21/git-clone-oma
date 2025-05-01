fb1f1e20848dc44f167bd182f6464a0d0a766309
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-01 16:20:32 +0200

    drop the constrain on the down migration

--- internal/migrations/0002_add_filename.down.sql
+++ internal/migrations/0002_add_filename.down.sql
@@ -1,2 +1,4 @@
-alter table drop column if exists filename
+alter table repositories drop column if exists filename;
+
+alter table repositories drop constraint oma_repo_id_unique_for_each_filename
 
