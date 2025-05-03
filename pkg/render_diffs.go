d8a66fdb2cb497a21088e86caa41aa9f5d9bd10b
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-04 01:43:07 +0200

    make use of the new diff return and add planned features-fixes-changes as comments

--- pkg/render_diffs.go
+++ pkg/render_diffs.go
@@ -16,16 +16,20 @@ func isVisuallyEmpty(s string) bool {
 }
 
 // I couldn't find a nice table rendering display tool for large text files
-// (or couldn't make tablewriter work in other words)
+// (or couldn't make tablewriter work in other words :D)
 // so here we go:
 func renderSideBySideDiff(oldColoured, newColoured, filename string) {
 	separator := Reset + " | " + Reset
 	oldLines, newLines := strings.Split(oldColoured, "\n"), strings.Split(newColoured, "\n")
 	maxLines := max(len(oldLines), len(newLines))
 
+	// FIXME: don't do this, it'll be confusing
+	// if there are files with same names within the same project
 	filenameBasePath := filepath.Base(filename)
 	headerName := consolidateShortLine(filenameBasePath)
 
+	// TODO: break down headers into Width length string slices as well,
+	// maybe they'll get too long due to their absolute path
 	fmt.Printf("\n%s%s%s\n", headerName, separator, headerName)
 
 	columnSeparator := strings.Repeat("-", Width*2)
@@ -34,6 +38,9 @@ func renderSideBySideDiff(oldColoured, newColoured, filename string) {
 	for i := range maxLines {
 		oldLine, newLine := oldLines[i], newLines[i]
 
+		// TODO: check if rendering an empty red line for emty oldLine
+		// and a green line for empty newLine makes sense or not?
+		// I don't remember how well this works for just a line addition or removal
 		if isVisuallyEmpty(oldLine) && isVisuallyEmpty(newLine) {
 			continue
 		}
@@ -43,14 +50,14 @@ func renderSideBySideDiff(oldColoured, newColoured, filename string) {
 }
 
 func RenderDiffs(oldContent, newContent, filename string) error {
-	additions, deletions, moves, normalizedOld, normalizedNew, err := GetDiff(oldContent, newContent)
+	diffResult := GetDiff(oldContent, newContent)
 
-	if err != nil {
-		return fmt.Errorf("diff view would be broken therefore it won't be shown for this file: %v\n", err)
+	if diffResult.error != nil {
+		return fmt.Errorf("diff view would be broken therefore it won't be shown for this file: %v\n", diffResult.error)
 	}
 
-	if len(additions) > 0 || len(deletions) > 0 || len(moves) > 0 {
-		oldColoured, newColoured := ColourTheDiffs(additions, deletions, moves, normalizedOld, normalizedNew)
+	if len(diffResult.additions) > 0 || len(diffResult.deletions) > 0 || len(diffResult.moves) > 0 {
+		oldColoured, newColoured := ColourTheDiffs(diffResult.additions, diffResult.deletions, diffResult.moves, diffResult.normalizedOld, diffResult.normalizedNew)
 		renderSideBySideDiff(oldColoured, newColoured, filename)
 	}
 
