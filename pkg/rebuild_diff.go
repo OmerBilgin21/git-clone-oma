2ce7be9549f5d46297bbdb71d9f01f73a22b0d2b
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-08 18:02:35 +0200

    remove consolidation and revert mode logic as it is done in different places now

--- pkg/rebuild_diff.go
+++ pkg/rebuild_diff.go
@@ -1,60 +1,24 @@
 package pkg
 
 import (
-	"fmt"
 	"oma/internal/storage"
 	"slices"
 	"strings"
 )
 
-func reconcileVersionActionsAfterApply(remaining []storage.VersionActions, wasDone storage.VersionActions, reverseMode bool) []storage.VersionActions {
-	reconciled := remaining
-	// TODO: pretty obvious, invert
-	if reverseMode {
-		fmt.Printf("not implemented yet!\n")
-	} else {
-		for i, action := range remaining {
-			if wasDone.ActionKey == storage.DeletionKey {
-				if (action.ActionKey == storage.AdditionKey || action.ActionKey == storage.DeletionKey) && action.Dest >= wasDone.Dest {
-					temp := remaining[i]
-					remaining = slices.Delete(remaining, i, i+1)
-					temp.Dest--
-					remaining = slices.Insert(remaining, i, temp)
-				}
-			} else if wasDone.ActionKey == storage.AdditionKey {
-				if (action.ActionKey == storage.AdditionKey || action.ActionKey == storage.DeletionKey) && action.Dest >= wasDone.Dest {
-					temp := remaining[i]
-					remaining = slices.Delete(remaining, i, i+1)
-					temp.Dest++
-					remaining = slices.Insert(remaining, i, temp)
-				}
-			}
-		}
-	}
-
-	return reconciled
-}
-
-func RecursiveRebuildDiff(oldArr []string, versionActions []storage.VersionActions, newVersion *string, revertMode bool) bool {
-	// TODO: again, pretty obvious
-	if len(versionActions) > 0 && revertMode {
-		fmt.Printf("not implemented yet!\n")
-	} else if len(versionActions) > 0 && !revertMode {
+func RebuildDiff(oldArr []string, versionActions []storage.VersionActions, newVersion *string) bool {
+	if len(versionActions) > 0 {
 		action := versionActions[0]
 
 		if action.ActionKey == storage.DeletionKey {
-			oldArr = slices.Delete(oldArr, action.Dest, action.Dest+1)
+			oldArr = slices.Delete(oldArr, action.Pos, action.Pos+1)
 		} else if action.ActionKey == storage.AdditionKey {
-			oldArr = slices.Insert(oldArr, action.Dest, action.Content)
-		} else {
-			oldArr = slices.Delete(oldArr, action.Start.V, action.Start.V+1)
-			oldArr = slices.Insert(oldArr, action.Dest, action.Content)
+			oldArr = slices.Insert(oldArr, action.Pos, action.Content)
 		}
 
 		remainders := slices.Delete(versionActions, 0, 1)
-		reconciled := reconcileVersionActionsAfterApply(remainders, action, revertMode)
 		*newVersion = strings.Join(oldArr, "\n")
-		if res := RecursiveRebuildDiff(oldArr, reconciled, newVersion, revertMode); res {
+		if res := RebuildDiff(oldArr, remainders, newVersion); res {
 			return true
 		}
 	}
