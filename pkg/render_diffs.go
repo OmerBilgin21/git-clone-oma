0158778d8349abe86ee5349d49633ef804b754ca
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-08 18:37:11 +0200

    do not use base path in the file name section of diffs

--- pkg/render_diffs.go
+++ pkg/render_diffs.go
@@ -2,7 +2,6 @@ package pkg
 
 import (
 	"fmt"
-	"path/filepath"
 	"strings"
 )
 
@@ -23,13 +22,10 @@ func renderSideBySideDiff(oldColoured, newColoured, filename string) {
 	oldLines, newLines := strings.Split(oldColoured, "\n"), strings.Split(newColoured, "\n")
 	maxLines := max(len(oldLines), len(newLines))
 
-	// FIXME: don't do this, it'll be confusing
-	// if there are files with same names within the same project
-	filenameBasePath := filepath.Base(filename)
-	headerName := consolidateShortLine(filenameBasePath)
+	fa := strings.Split(filename, "")
+	recursiveEqualizer(&fa)
+	headerName := strings.Join(fa, "")
 
-	// TODO: break down headers into Width length string slices as well,
-	// maybe they'll get too long due to their absolute path
 	fmt.Printf("\n%s%s%s\n", headerName, separator, headerName)
 
 	columnSeparator := strings.Repeat("-", Width*2)
@@ -38,9 +34,6 @@ func renderSideBySideDiff(oldColoured, newColoured, filename string) {
 	for i := range maxLines {
 		oldLine, newLine := oldLines[i], newLines[i]
 
-		// TODO: check if rendering an empty red line for emty oldLine
-		// and a green line for empty newLine makes sense or not?
-		// I don't remember how well this works for just a line addition or removal
 		if isVisuallyEmpty(oldLine) && isVisuallyEmpty(newLine) {
 			continue
 		}
