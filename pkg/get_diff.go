e6375313c28845208cf6628b9f8d03fa3b137679
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-05-08 18:01:41 +0200

    change diff alg to only search for removes and adds

--- pkg/get_diff.go
+++ pkg/get_diff.go
@@ -1,23 +1,21 @@
 package pkg
 
 import (
+	"oma/internal/storage"
 	"slices"
 	"strings"
 )
 
 type Action struct {
-	from    int
-	to      int
-	content string
+	Pos        int
+	Content    string
+	ActionType storage.Keys
 }
 
 type DiffResult struct {
-	additions     []Action
-	deletions     []Action
-	moves         []Action
-	normalizedOld string
-	normalizedNew string
-	error         error
+	Actions       []Action
+	NormalizedOld string
+	NormalizedNew string
 }
 
 func GetDiff(oldStr string, newStr string, visualMode bool) DiffResult {
@@ -31,85 +29,54 @@ func GetDiff(oldStr string, newStr string, visualMode bool) DiffResult {
 		oldArr, newArr = strings.Split(oldStr, "\n"), strings.Split(newStr, "\n")
 	}
 
-	var additions []Action
-	var deletions []Action
-	var moves []Action
+	temp := slices.Clone(oldArr)
+	var actions []Action
 
-	oldMap := make(map[string]int)
-	newMap := make(map[string]int)
-
-	for i, l := range oldArr {
-		oldMap[l] = i
-	}
-
-	for i, l := range newArr {
-		newMap[l] = i
-	}
-
-	for x, n := range newArr {
-		if _, exists := oldMap[n]; !exists {
-			additions = append(additions, Action{
-				to:      x,
-				content: n,
-			})
+	newCtr, tempCtr := 0, 0
+	for newCtr < len(newArr) && tempCtr < len(temp) {
+		if newArr[newCtr] == temp[tempCtr] {
+			newCtr++
+			tempCtr++
+			continue
 		}
-	}
-
-	for y, o := range oldArr {
-		if _, exists := newMap[o]; !exists {
-			deletions = append(deletions, Action{
-				to:      y,
-				content: o,
-			})
-		}
-	}
-
-	// in order to properly understand what is really moved, we need a temp version of the old
-	// string where we apply the additions and deletions first
-	temp := oldArr
-	for _, add := range additions {
-		temp = slices.Insert(temp, add.to, add.content)
-	}
-
-	// fmt.Printf("deletions: %+v\n", deletions)
-	for _, del := range deletions {
-		temp = slices.Delete(temp, del.to, del.to+1)
-	}
-
-	tempMap := make(map[string]int)
-
-	for i, l := range temp {
-		tempMap[l] = i
-	}
-
-	for _, n := range newArr {
-		if _, exists := tempMap[n]; exists && (tempMap[n] != newMap[n]) {
-			toBeAdded := Action{from: tempMap[n], to: newMap[n], content: n}
-			skip := false
-			for _, move := range moves {
-				if move.from == toBeAdded.to && move.to == toBeAdded.from {
-					skip = true
-				}
-			}
-			if !skip {
-				moves = append(moves, toBeAdded)
-				// in order to keep track of what was shifted to where,
-				// after each move, we have to be continuously updating the temp/tempMap
-				temp = slices.Delete(temp, toBeAdded.from, toBeAdded.from+1)
-				temp = slices.Insert(temp, toBeAdded.to, toBeAdded.content)
-				for i, l := range temp {
-					tempMap[l] = i
-				}
+		found := -1
+		for j := tempCtr + 1; j < len(temp); j++ {
+			if temp[j] == newArr[newCtr] {
+				found = j
+				break
 			}
 		}
+		if found >= 0 {
+			// temp[tempCtr:found] are deletions
+			for k := tempCtr; k < found; k++ {
+				actions = append(actions, Action{Pos: tempCtr, Content: temp[tempCtr], ActionType: storage.DeletionKey})
+				temp = slices.Delete(temp, tempCtr, tempCtr+1)
+			}
+		} else {
+			// couldn't find the newStr's current line in the rest of temp, add all of em
+			actions = append(actions, Action{Pos: tempCtr, Content: newArr[newCtr], ActionType: storage.AdditionKey})
+			temp = slices.Insert(temp, tempCtr, newArr[newCtr])
+			newCtr++
+			tempCtr++
+		}
+	}
+
+	// ran out of newArr, delete leftovers
+	for tempCtr < len(temp) {
+		actions = append(actions, Action{Pos: tempCtr, Content: temp[tempCtr], ActionType: storage.DeletionKey})
+		temp = slices.Delete(temp, tempCtr, tempCtr+1)
+	}
+
+	// ran out of temp, insert remaining
+	for ; newCtr < len(newArr); newCtr++ {
+		actions = append(actions, Action{Pos: tempCtr, Content: newArr[newCtr], ActionType: storage.AdditionKey})
+		temp = slices.Insert(temp, tempCtr, newArr[newCtr])
+		tempCtr++
 	}
 
 	return DiffResult{
-		additions:     additions,
-		deletions:     deletions,
-		moves:         moves,
-		normalizedOld: normalizedOld,
-		normalizedNew: normalizedNew,
-		error:         nil,
+		Actions:       actions,
+		NormalizedOld: normalizedOld,
+		NormalizedNew: normalizedNew,
 	}
 }
